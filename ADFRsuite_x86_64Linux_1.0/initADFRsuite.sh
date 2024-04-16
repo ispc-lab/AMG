60e63b8a936e5dfc2dd9827f9a701d8f131cb097
XudongZhang <105139522+IsXudongZhang@users.noreply.github.com>
2024-04-16 19:49:54 +0800

    v1

---
+++ ADFRsuite_x86_64Linux_1.0/initADFRsuite.sh
@@ -0,0 +1,8 @@
+alias agfr='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/agfr'
+alias adfr='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/adfr'
+alias agfrgui='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/agfrgui'
+alias autosite='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/autosite'
+alias about='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/about'
+alias prepare_receptor='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/prepare_receptor'
+alias prepare_ligand='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/prepare_ligand'
+alias pythonsh='/home/east/projects/ScaRLPR/ADFRsuite_x86_64Linux_1.0/bin/pythonsh'
---
+++ README.md
@@ -0,0 +1,2 @@
+# AMG
+ The official repository of our paper "Deep reinforcement learning as an interaction agent to steer fragment-based 3D molecular generation for protein pockets"
