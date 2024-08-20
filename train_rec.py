c80053d93845c1a8b1913e218fdc08653a6bed30
east <east@tongji.edu.cn>
2024-08-20 20:29:18 +0800

    init

--- train_rec.py
+++ train_rec.py
@@ -69,9 +69,6 @@ def setup_model(config, pocket_featurizer, ligand_featurizer, vocab, args):
         ckpt_ligand_path=args.PR_ligand_path,
         ckpt_pocket_path=args.PR_pocket_path,
         device=args.device).to(args.device)
-    if args.PR_path:
-        ckpt = torch.load(args.PR_path, map_location=args.device)
-        model.load_state_dict(ckpt['model'])
     return model
 
 
@@ -173,162 +170,13 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, default='./configs/train_model.yml')
-    parser.add_argument('--device', type=str, default='cuda:0')
-    parser.add_argument('--logdir', type=str, default='./logs/new_dataset_AMG/recovery/')
-    parser.add_argument('--temp_dir', type=str, default='/home/east/projects/Sca/temp_files')
+    parser.add_argument('--device', type=str, default='cuda:1')
+    parser.add_argument('--logdir', type=str, default='./logs/ablations/wo_ligandPR/')
     parser.add_argument('--vocab_path', type=str, default='dataset/vocab_pretrain.txt')
-    parser.add_argument('--PR_path', type=str, default=None, help='Load pretraining representation of ligand and pocket')
-    parser.add_argument('--PR_ligand_path', type=str, default='logs/PretrainLigand/vocab_np/pretrain_ligand_2023_10_26__00_05_27/checkpoints/1000000.pt', help='Load pretraining representation of ligand')
-    parser.add_argument('--PR_pocket_path', type=str, default='logs/pocket_pretrain_logs/pretrain_pocket_2023_10_26__10_04_48/checkpoints/1000000.pt', help='Load pretraining representation of pocket')
+    parser.add_argument('--PR_ligand_path', type=str, default=None, 
+                        help='Load pretrained model of ligand')
+    parser.add_argument('--PR_pocket_path', type=str, default='./ckpts/pretrained_pocket_ckpt.pt', 
+                        help='Load pretrained model of pocket')
     args = parser.parse_args()
 
     main(args)
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--config', type=str, default='./configs/train_model.yml')
-#     parser.add_argument('--device', type=str, default='cuda:0')
-#     parser.add_argument('--logdir', type=str, default='./logs/new_dataset_AMG/recovery/')
-#     parser.add_argument('--temp_dir', type=str, default='/home/east/projects/Sca/temp_files')
-#     parser.add_argument('--vocab_path', type=str, default='dataset/vocab_pretrain.txt')
-#     parser.add_argument('--PR_path', type=str, default=None, help='Load pretraining representation of ligand and pocket')                 # '/media/data2/xudong/logs/fine_tuning_logs/train_model_2023_09_10__20_19_52/checkpoints/920000.pt')          # None or load path
-#     parser.add_argument('--PR_ligand_path', type=str, default='logs/PretrainLigand/vocab_np/pretrain_ligand_2023_10_26__00_05_27/checkpoints/1000000.pt', help='Load pretraining representation of ligand')              # '/media/data2/xudong/logs/ligand_pretrain_logs/train_model_2023_09_14__19_00_44/checkpoints/100000.pt')          # None or load path
-#     parser.add_argument('--PR_pocket_path', type=str, default='logs/pocket_pretrain_logs/pretrain_pocket_2023_10_26__10_04_48/checkpoints/1000000.pt', help='Load pretraining representation of pocket')          # None or load path
-#     args = parser.parse_args()
-
-#     # Load vocab
-#     vocab = []
-#     for line in open(args.vocab_path):
-#         p, _, _ = line.partition(':')
-#         vocab.append(p)
-#     vocab = Vocab(vocab)
-
-#     # Load configs
-#     config = load_config(args.config)
-#     config_name = os.path.basename(args.config)[:os.path.basename(args.config).rfind('.')]
-#     seed_all(config.train.seed)
-
-#     # Logging
-#     log_dir = get_new_log_dir(args.logdir, prefix=config_name)
-#     ckpt_dir = os.path.join(log_dir, 'checkpoints')
-#     os.makedirs(ckpt_dir, exist_ok=True)
-#     logger = get_logger('train', log_dir)
-#     writer = torch.utils.tensorboard.SummaryWriter(log_dir)
-#     logger.info(args)
-#     logger.info(config)
-#     shutil.copyfile(args.config, os.path.join(log_dir, os.path.basename(args.config)))
-#     shutil.copytree('./models', os.path.join(log_dir, 'models'))
-
-#     # Transforms
-#     pocket_featurizer = FeaturizePocketAtom()
-#     ligand_featurizer = FeaturizeLigandAtom()
-#     masking = get_mask(config.train.transform.mask, vocab)
-#     transform = Compose([
-#         LigandCountNeighbors(),
-#         pocket_featurizer,
-#         ligand_featurizer,
-#         FeaturizeLigandBond(),
-#         masking,
-#     ])
-
-#     # Datasets and loaders
-#     logger.info('Loading dataset...')
-#     dataset, subsets = get_dataset(config=config.dataset, transform=transform)
-#     train_set, val_set = subsets['train'], subsets['test']
-#     train_iterator = inf_iterator(DataLoader(train_set, batch_size=config.train.batch_size,
-#                                              shuffle=True, num_workers=config.train.num_workers,
-#                                              collate_fn=collate_pocket_ligand))
-#     val_loader = DataLoader(val_set, batch_size=config.train.batch_size, shuffle=False,
-#                             num_workers=config.train.num_workers, collate_fn=collate_pocket_ligand, drop_last=True)
-
-#     # Model
-#     logger.info('Building model...')
-#     if args.PR_path:
-#         ckpt = torch.load(args.PR_path, map_location=args.device)
-#     model = AMG(
-#         config.model,
-#         pocket_atom_feature_dim=pocket_featurizer.feature_dim,
-#         ligand_atom_feature_dim=ligand_featurizer.feature_dim,
-#         vocab=vocab,
-#         ckpt_ligand_path=args.PR_ligand_path,
-#         ckpt_pocket_path=args.PR_pocket_path,
-#         device=args.device).to(args.device)
-#     if args.PR_path:
-#         model.load_state_dict(ckpt['model'])
-
-#     # Optimizer and scheduler
-#     optimizer = get_optimizer(config.train.optimizer, model)
-#     scheduler = get_scheduler(config.train.scheduler, optimizer)
-
-
-#     def train(it):
-#         model.train()
-#         optimizer.zero_grad()
-        
-#         batch = next(train_iterator)
-
-#         for key in batch:
-#             if key not in ['pocket_filename', 'ligand_filename']:
-#                 batch[key] = batch[key].to(args.device)
-
-#         loss, loss_list = model.get_loss(batch)
-
-#         loss.backward()
-#         orig_grad_norm = clip_grad_norm_(model.parameters(), config.train.max_grad_norm)
-#         optimizer.step()
-
-#         logger.info('[Train] Iter %d | Loss %.6f | Loss(Interaction) %.6f | Loss(Pred) %.6f | Loss(comb) %.6f | Loss(Focal) %.6f | Loss(Dm) %.6f '
-#                     '| Loss(Tor) %.6f | Loss(Recovery) %.6f  | Orig_grad_norm %.6f' % (it, loss.item(), loss_list[0], loss_list[1], loss_list[2], loss_list[3], loss_list[4], loss_list[5], loss_list[6], orig_grad_norm))
-#         writer.add_scalar('train/loss', loss, it)
-#         writer.add_scalar('train/interaction_loss', loss_list[0], it)
-#         writer.add_scalar('train/pred_loss', loss_list[1], it)
-#         writer.add_scalar('train/comb_loss', loss_list[2], it)
-#         writer.add_scalar('train/focal_loss', loss_list[3], it)
-#         writer.add_scalar('train/dm_loss', loss_list[4], it)
-#         writer.add_scalar('train/torsion_loss', loss_list[5], it)
-#         writer.add_scalar('train/recovery_loss', loss_list[6], it)
-#         writer.add_scalar('train/lr', optimizer.param_groups[0]['lr'], it)
-#         writer.add_scalar('train/grad', orig_grad_norm, it)
-#         writer.flush()
-
-
-#     def validate(it):
-#         sum_loss, sum_n = 0, 0
-#         with torch.no_grad():
-#             model.eval()
-#             for batch in tqdm(val_loader, desc='Validate'):
-#                 for key in batch:
-#                     if key not in ['pocket_filename', 'ligand_filename']:
-#                         batch[key] = batch[key].to(args.device)
-#                 loss, _ = model.get_loss(batch)
-#                 sum_loss += loss.item()
-#                 sum_n += 1
-#         avg_loss = sum_loss / sum_n
-
-#         if config.train.scheduler.type == 'plateau':
-#             scheduler.step(avg_loss)
-#         elif config.train.scheduler.type == 'warmup_plateau':
-#             scheduler.step_ReduceLROnPlateau(avg_loss)
-#         else:
-#             scheduler.step()
-
-#         logger.info('[Validate] Iter %05d | Loss %.6f' % (it, avg_loss,))
-#         writer.add_scalar('val/loss', avg_loss, it)
-#         writer.flush()
-#         return avg_loss
-
-#     for it in range(1, config.train.max_iters + 1):
-#     # for it in range(1000000, config.train.max_iters + 1000001):
-#         train(it)
-#         if it % config.train.val_freq == 0 or it == config.train.max_iters:
-#             validate(it)
-#             ckpt_path = os.path.join(ckpt_dir, '%d.pt' % it)
-#             torch.save({
-#                 'config': config,
-#                 'model': model.state_dict(),
-#                 'optimizer': optimizer.state_dict(),
-#                 'scheduler': scheduler.state_dict(),
-#                 'iteration': it,
-#             }, ckpt_path)
-#             torch.cuda.empty_cache()
